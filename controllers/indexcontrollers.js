c717bd464a0d7b99970f93b606c0455449290fc6
Prikshit <prikshit1028@gmail.com>
2025-11-28 00:01:29 +0530

    form validation added, however had to remove the ssl configuration for it work locally

--- controllers/indexcontrollers.js
+++ controllers/indexcontrollers.js
@@ -1,16 +1,34 @@
 const dbcontroller = require("../db/queries");
+const { body, validationResult, matchedData } = require("express-validator");
+
+const validation = [
+  body("author_name")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Name cannot be empty"),
+  body("message_text")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Message cannot be empty"),
+];
 
 exports.homepage = async function (req, res) {
   let result = await dbcontroller.getMessages();
   res.render("index", { messages_all: result });
 };
 
-exports.postmessage = async function (req, res) {
-  let name = req.body.author_name;
-  let msg = req.body.message_text;
-  await dbcontroller.postmessage(name, msg);
-  res.redirect("/");
-};
+exports.postmessage = [
+  validation,
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("form", { erro: errors.array() });
+    }
+    const { author_name, message_text } = matchedData(req);
+    await dbcontroller.postmessage(author_name, message_text);
+    res.redirect("/");
+  },
+];
 
 exports.getMessage = async function (req, res) {
   let idof = req.params.id;
